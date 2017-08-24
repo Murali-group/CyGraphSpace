4f24d46265d576065608333a28b185bce1bb14d9
Rishabh Sethi <rishu_sethi1@yahoo.co.in>
2017-08-25 01:07:15 +0530

    removed style import and login persistence functionality

--- CyGraphSpaceApp/src/main/java/org/cytoscape/graphspace/cygraphspace/internal/util/PropertyReader.java
+++
@@ -1,9 +0,0 @@
-package org.cytoscape.graphspace.cygraphspace.internal.util;
-
-import org.cytoscape.property.AbstractConfigDirPropsReader;
-
-public class PropertyReader extends AbstractConfigDirPropsReader{
-	public PropertyReader(String name, String propFileName) {
-		super(name, propFileName, SavePolicy.CONFIG_DIR);
-	}
-}

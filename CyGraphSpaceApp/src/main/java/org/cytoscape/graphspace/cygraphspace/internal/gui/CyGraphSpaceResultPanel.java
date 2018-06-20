a04b36cfc48e1a5cb82f931dd70227f4e81c1169
li <lhuang210@gmail.com>
2018-06-19 20:58:31 -0400

    Refs #20: Create basic panel structure to hold cygraphspace tasks.

--- CyGraphSpaceApp/src/main/java/org/cytoscape/graphspace/cygraphspace/internal/gui/CyGraphSpaceResultPanel.java
+++ CyGraphSpaceApp/src/main/java/org/cytoscape/graphspace/cygraphspace/internal/gui/CyGraphSpaceResultPanel.java
@@ -1,9 +1,13 @@
 package org.cytoscape.graphspace.cygraphspace.internal.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.Icon;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import org.cytoscape.application.swing.CytoPanelComponent;
 import org.cytoscape.application.swing.CytoPanelName;
@@ -12,9 +16,25 @@ import org.cytoscape.application.swing.CytoPanelName;
 public class CyGraphSpaceResultPanel extends JPanel implements CytoPanelComponent {
 
 	private String title;
+	private JPanel mainList;
 
 	public CyGraphSpaceResultPanel(String title) {
 		this.title = title;
+		initializePanel();
+	}
+
+	private void initializePanel() {
+		setLayout(new BorderLayout());
+
+		// based on https://stackoverflow.com/questions/14615888/list-of-jpanels-that-eventually-uses-a-scrollbar
+		this.mainList = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
+		mainList.add(new JPanel(), gbc);
+
+		add(new JScrollPane(mainList));
 	}
 
 	@Override
